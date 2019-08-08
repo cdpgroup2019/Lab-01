d04444c558e734842ee9e66179492db0f744b9d9
Randheerkumar <randheerdpr@gmail.com>
2019-08-08 23:37:58 +0530

    q2 qnd q4

---
+++ Ass1_Q4/a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int x;
+int y=5;
+void fun();
+
+int main(int argc, char const *argv[])
+{
+	fun();
+	return 0;
+}
+
+void fun(){
+   int c;
+   char *name;
+   name=(char*)malloc(2*sizeof(char));
+   printf("Main @ %lx\n",(long unsigned int)&main);
+   printf("Recursive @ %lx\n",(long unsigned int)&fun);
+   printf("Uninilized G @ %lx\n",(long unsigned int)&x);
+   printf("Initialized G @ %lx\n",(long unsigned int)&y);
+   printf("Local @ %lx\n",(long unsigned int)&c);
+   printf("Malloc @ %lx\n",(long unsigned int)&name);
+
+   fun();
+}
